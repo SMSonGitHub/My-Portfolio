df8714ac63894895539659a28c4738d578d2a03e
SMSonGitHub <sabienruffin@gmail.com>
2021-10-04 16:43:16 -0500

    added useEffect/Ref examples, animated text.

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,41 @@
 import "./intro.scss"
+import { init } from 'ityped'
+import { useEffect, useRef } from "react"
 
 export default function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current,
+        {   showCursor: true,
+            backDelay: 1500,
+            backSpeed: 60,
+            strings: ["Developer", "Designer", "Content Creator"],
+            //cursorChar: "|",
+        });
+    }, [])
+
+
     return (
         <div className="intro" id="intro">
-            squee
+                    <div className="left">
+                        <div className="imgContainer">
+                        <img src="\assets\Zay1.png" alt=""/>
+                        </div>
+                    </div>
+                    <div className="right">
+                        <div className="wrapper">
+                            <h2> Discover The</h2>
+                            <h1> SX Brand of Coding!</h1>
+                            <h3>Freelance <span ref={textRef}></span></h3>
+                        </div>
+                        <a href="#portfolio">
+                            <img src="assets\down.png" alt=""/>
+                        </a>
+                    </div>
+                    
+                
         </div>
     )
 }
--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default function Works() {
     return (
-        <div className="works" id="Works">
+        <div className="works" id="works">
             Works
         </div>
     )
