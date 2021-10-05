cfabd61461ffff56c522adeb4f32da4850519e90
SMSonGitHub <sabienruffin@gmail.com>
2021-10-05 02:23:28 -0500

    Updated Portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,21 @@ import "./portfolio.scss"
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
-            Portfolio
+            <h1> Portfolio </h1>
+
+             <ul>
+                <li className="active">Featured</li>
+                <li>Web App</li>
+                <li>Mobile App</li>
+                <li>Design</li>
+                <li>Branding</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="" alt="placeholder"/>
+                    <h3>GSUA</h3>
+                </div>
+            </div>
         </div>
     )
 }
