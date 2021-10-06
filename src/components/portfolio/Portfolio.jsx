1f28e11f6ef47ddbcf86e641303d1cc5b284802c
SMSonGitHub <sabienruffin@gmail.com>
2021-10-06 00:51:58 -0500

    addtl useState/Effect testing

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,20 +1,50 @@
-import "./portfolio.scss"
+import "./portfolio.scss";
+import PortfolioList from "../portfolioList/PortfolioList";
+import {useEffect, useState} from "react";
+
 
 export default function Portfolio() {
+    const [selected,setSelected] = useState("featured")
+    const list = [ 
+    {
+        id: "featured",
+        title: "Featured",
+    }, 
+    {
+        id: "web",
+        title: "Web App",
+    }, 
+    {
+        id: "mobile",
+        title: "Mobile App",
+    }, 
+    {
+        id: "design",
+        title: "Design",
+    }, 
+    {
+        id: "content",
+        title: "Content",
+    }, 
+ ];
+
     return (
         <div className="portfolio" id="portfolio">
             <h1> Portfolio </h1>
 
              <ul>
-                <li className="active">Featured</li>
-                <li>Web App</li>
-                <li>Mobile App</li>
-                <li>Design</li>
-                <li>Branding</li>
+                {list.map((item) => (
+                    <PortfolioList title={item.title} 
+                    active = {selected === item.id}
+                    setSelected ={setSelected}
+                    id={item.id}
+                    />
+                    
+                ))}
             </ul>
             <div className="container">
                 <div className="item">
-                    <img src="" alt="placeholder"/>
+                    <img src="assets\sxgreen.PNG" height= "128px" alt="placeholder"/>
                     <h3>GSUA</h3>
                 </div>
             </div>
