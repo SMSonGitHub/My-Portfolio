c81effa2818e93437899a00c995828c3b8e2e173
SMSonGitHub <sabienruffin@gmail.com>
2021-10-13 23:15:14 -0500

    Initiated work on testimonial cards
    Added transition to work cards

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -3,7 +3,24 @@ import "./testimonials.scss"
 export default function Testimonials() {
     return (
         <div className="testimonials" id="testimonials">
-            Testimonials
+            <h1>Testimonials </h1>
+            <div className="container">
+                <div className="card">
+                    <div className="top">
+                        <img src ="assets/down.png" className="left" alt=""/>
+                        <img src ="assets/sxgreen.png" className="user" alt=""/>
+                        <img src ="assets/youtube.png" className="right" alt=""/>  
+                    </div>
+                    <div className="center">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    </div>
+                    <div className="bottom">
+                    <h3> S. Xavier </h3>
+                    <h4> Developer </h4>
+                    </div>
+                    
+                </div>
+            </div>
         </div>
     )
 }
