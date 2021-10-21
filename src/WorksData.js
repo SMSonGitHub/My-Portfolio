ceb5bc2dc6dd289cb54b8812473803f18064a693
SMSonGitHub <sabienruffin@gmail.com>
2021-10-21 17:09:27 -0500

    Bugfix
    Fixed a bug where certain images/icons would not appear correctly

--- src/WorksData.js
+++ src/WorksData.js
@@ -2,44 +2,44 @@ export const data = [
     
 {
     id: "1",
-    icon: "./assets/sxgreen.png",
+    icon: "./assets/sxgreen.PNG",
     title: "Featured",
     desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
     img: "./assets/sxgreen.PNG"
 }, 
 {
     id: "2",
-    icon: "./assets/sxyellow.png",
+    icon: "./assets/sxyellow.PNG",
     title: "Web App",
     desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    img: "./assets/sxyellow.png"
+    img: "./assets/sxyellow.PNG"
 }, 
 {
     id: "3",
-    icon: "./assets/sxgreen.png",
+    icon: "./assets/sxgreen.PNG",
     title: "Mobile App",
     desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    img: "./assets/sxgreen.png"
+    img: "./assets/sxgreen.PNG"
 }, 
 {
     id: "4",
-    icon: "./assets/sxyellow.png",
+    icon: "./assets/sxyellow.PNG",
     title: "Design",
     desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    img: "./assets/sxyellow.png"
+    img: "./assets/sxyellow.PNG"
 }, 
 {
     id: "5",
-    icon: "./assets/sxgreen.png",
+    icon: "./assets/sxgreen.PNG",
     title: "Content",
     desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    img: "./assets/sxgreen.png"
+    img: "./assets/sxgreen.PNG"
 }, 
 {
     id: "6",
-    icon: "./assets/sxyellow.png",
+    icon: "./assets/sxyellow.PNG",
     title: "Contact",
     desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    img: "./assets/sxyellow.png"
+    img: "./assets/sxyellow.PNG"
 },
 ]
